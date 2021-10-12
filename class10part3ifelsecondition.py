5aba77d47157ba95c7bd23085718e4b9369d78a1
sudita07 <92347854+sudita07@users.noreply.github.com>
2021-10-11 19:51:05 -0700

    Add files via upload

---
+++ class10part3ifelsecondition.py
@@ -0,0 +1,27 @@
+# program to find odd or even numbers
+# x=int(input("enter your number"))
+# r=x%2
+# if(r==0):
+#     print("even")
+# else:
+#     print("odd")
+
+# x=int(input("enter first number"))
+# if x>=0:
+#     print("is positive")
+# else:
+#     print("is negative")
+
+# x=int(input("enter age"))
+# if x>=60:
+#     print("senior citizen")
+# else:
+#     print("is young")
+
+v=int(input("enter num"))
+b=int(input("enter 2nd num"))
+if v<b:
+    print("v is smaller")
+else:
+    print("b is smaller")
+
